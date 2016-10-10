45ea203bd14fb0b0d8b23ad2af9c3e1c642bc75b
brandongq <brandongq@gmail.com>
2016-10-09 22:16:50 -0700

    test

--- slackMessage/handler.js
+++ slackMessage/handler.js
@@ -1,32 +1,42 @@
-console.log('Loading function');
+'use strict';
 
-var https = require('https');
-var url = require('url');
+var request = require('request');
+/*
+const https = require('https');
+const url = require('url');
 // to get the slack hook url, go into slack admin and create a new "Incoming Webhook" integration
-var slack_url = 'https://hooks.slack.com/services/...';
-var slack_req_opts = url.parse(slack_url);
+var slack_url = 'https://hooks.slack.com/services/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';
+const slack_req_opts = url.parse(slack_url);
 slack_req_opts.method = 'POST';
 slack_req_opts.headers = {'Content-Type': 'application/json'};
 
-module.exports.handler = function(event, context) {
-  (event.Records || []).forEach(function (rec) {
-    if (rec.Sns) {
-      var req = https.request(slack_req_opts, function (res) {
-        if (res.statusCode === 200) {
-          context.succeed('posted to slack');
-        } else {
-          context.fail('status code: ' + res.statusCode);
+*/
+
+
+module.exports.handler = function(event, context, cb) {
+  var theJson = JSON.parse(event.Records[0].Sns.Message);
+  var slack_url = 'https://hooks.slack.com/services/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';
+  var commit = theJson.commits[0];
+  var theText = "commit with id {0}".format(commit.id);
+  var message = {
+    //"text": theJson.pusher.name + ":\n" + theJson.commits[0].message
+    //"text": theJson.message
+    "text": theText
+  };
+
+  request.post(slack_url,
+      {
+        json: true,
+        body: message
+      },
+      function (err, response, body) {
+        if(err || response.statusCode != 200) {
+          console.log(err, response);
         }
+        else{
+          console.log(body);
+        }
+        context.succeed();
       });
 
-      req.on('error', function(e) {
-        console.log('problem with request: ' + e.message);
-        context.fail(e.message);
-      });
-
-      req.write(JSON.stringify({text: JSON.stringify(rec.Sns.Message, null, '  ')})); // for testing: , channel: '@vadim'
-
-      req.end();
-    }
-  });
 };
